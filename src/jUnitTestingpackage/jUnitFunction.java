d0b725c9aade1533cc902f281a4353b9eb71d4ce
HP <HP@Ritvik>
2017-09-27 18:56:02 +0530

    1

---
+++ src/jUnitTestingpackage/jUnitFunction.java
@@ -0,0 +1,12 @@
+package jUnitTestingpackage;
+
+public class jUnitFunction {
+	public int addNumber(int num1, int num2){
+		return num1+num2;
+	}
+	public String assStrings(String s1, String s2){
+		return s1+s2;
+	}
+	
+}
+
